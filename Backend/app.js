f53c39ec9adfea4bdea226745f9b8556a0cd44b1
hussain <mohussain237@gmail.com>
2022-03-21 06:50:39 +0000

    server side code fro Book_store

---
+++ Backend/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require("./routes/book-routes");
+const cors = require("cors");
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+app.use("/books", router); 
+
+
+// connection to DB
+mongoose
+  .connect(
+    'mongodb+srv://user01:MjmxfCplpsHI6zrZ@mydatabase.lojjr.mongodb.net/MACHINE_TEST?retryWrites=true&w=majority'
+  )
+  .then(() => console.log("Connected To Database"))
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((err) => console.log(err));
+
+
+  // Data for Books is copied from 'https://www.goodreads.com/list/show/252.Books_Made_into_Movies' & 'https://www.imdb.com/list/ls020599634/'
